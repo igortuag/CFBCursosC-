05b3f5bd9ab59fcb864aa576480b2ed79e29ced6
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-05 09:36:37 -0300

    Aula 08 entrada de dados pelo teclado.

---
+++ Aula08/Aula08.cs
@@ -0,0 +1,20 @@
+using System;
+class Aula08{
+    static void Main(){
+        int v1, v2, soma;
+        string nome;
+
+        System.Console.WriteLine("Digite o seu nome: ");
+        nome = Console.ReadLine();
+
+        System.Console.Write("Valor 01: ");
+        v1 = int.Parse(Console.ReadLine());
+
+        System.Console.Write("Valor 02: ");
+        v2 = int.Parse(Console.ReadLine());
+
+        soma = v1 + v2;
+
+        System.Console.WriteLine("Olá {0}, a soma entre {1} e {2} é igual a {3}",nome,v1,v2,soma);
+    }
+}
