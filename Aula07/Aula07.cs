54830621337383ba454ab2da40e000dd9c2c31ee
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-05 09:31:59 -0300

    Aula07 Constantes

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,9 @@
+using System;
+class Aula07{
+    static void Main(){
+        const string canal="CFB Cursos";
+        const double pi=3.1415;
+
+        System.Console.WriteLine("Canal {0}\nPi: {1}", canal, pi);
+    }
+}
