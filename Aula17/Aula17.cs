d7ff6de8812a6b494fb1b1468b032f98bfc41117
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-05 11:15:00 -0300

    Aula 17 Array / Vetor

---
+++ Aula17/Aula17.cs
@@ -0,0 +1,16 @@
+using System;
+class Aula17{
+    static void Main(){
+        int[] n = new int[5];
+        // int[] num = new int[3]{55, 77, 99};
+        int[] num = {55, 77, 99};
+
+        n[0] = 25;
+        n[1] = 111;
+        n[2] = 222;
+        n[3] = 333;
+        n[4] = 48756;
+
+        System.Console.WriteLine(num[1]);
+    }
+}
