a4ef9388435cabd3ca998c5ca1b74da356160afd
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-05 08:10:31 -0300

    Aula03

--- Aula03/aula03.cs
+++ Aula03/aula03.cs
@@ -1,6 +1,17 @@
 using System;
 class Aula03{
     static void Main(){
-        
+        byte n1=10; // 0 a 255
+        int num1 = 2; 
+        int num2 = 3;
+        int soma;
+        char letra = 'c';
+        float valor = 5.3f;
+        soma = num1 + num2;
+        System.Console.WriteLine(soma);
+        String nome = "Igor";
+
+        var aux=nome; // Operador var permite não especificar o tipo na declaração, irá ser determinado na hora da atribuição.
+        System.Console.WriteLine("Olá " +aux);
     }
 }
