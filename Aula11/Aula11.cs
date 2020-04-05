ebb846a34b0dfe5252da8304124cae4785d5c3b1
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-05 10:14:11 -0300

    Aula 11 typecast

---
+++ Aula11/Aula11.cs
@@ -0,0 +1,18 @@
+using System;
+class Aula11{
+    static void Main(){
+
+        float n1 = 180.4585f;
+        int n2 = (int)n1;
+        double n3 = n1;
+
+        short n4 = (short)n2;
+
+
+
+        System.Console.WriteLine(n2);
+        System.Console.WriteLine(n3);
+        System.Console.WriteLine(n4);
+
+    }
+}
