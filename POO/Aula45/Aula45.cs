7033d1dfeb1a1b3493b8d5b4d25c142cbf852d79
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-10 17:25:36 -0300

    Aula 45 Array de estruturas

---
+++ POO/Aula45/Aula45.cs
@@ -0,0 +1,35 @@
+using System;
+
+struct Carro{
+    public string modelo;
+    public string cor;
+    public void info(){
+        System.Console.WriteLine("Modelo.: {0}",this.modelo);
+        System.Console.WriteLine("Cor.: {0}",this.cor);
+        System.Console.WriteLine("-----------------------------------");
+    }
+}
+class Aula45{
+    static void Main(){
+        int[] numeros = new int[10];
+
+        Carro[] carros = new Carro[4];
+
+        carros[0].modelo = "HRV";
+        carros[0].cor = "Prata";
+
+        carros[1].modelo = "Golf";
+        carros[1].cor = "Azul";
+
+        carros[2].modelo = "Jetta";
+        carros[2].cor = "Branco";
+
+        carros[3].modelo = "Argo";
+        carros[3].cor = "Prata";
+
+        for (int i = 0; i < carros.Length; i++)
+        {
+            carros[i].info();
+        }
+    }
+}
