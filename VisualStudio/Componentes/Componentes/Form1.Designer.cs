2e5fd9458aa183879bfb52e06bec78139d1bd3dc
Igor Tuag <igor.tuag@ketra.com.br>
2020-04-11 15:48:12 -0300

    Aula 62 Componente Textbox

---
+++ VisualStudio/Componentes/Componentes/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Componentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_adicionar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_veiculo = new System.Windows.Forms.TextBox();
+            this.tb_listaVeiculos = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_adicionar
+            // 
+            this.btn_adicionar.Location = new System.Drawing.Point(188, 23);
+            this.btn_adicionar.Name = "btn_adicionar";
+            this.btn_adicionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_adicionar.TabIndex = 0;
+            this.btn_adicionar.Text = "Adicionar";
+            this.btn_adicionar.UseVisualStyleBackColor = true;
+            this.btn_adicionar.Click += new System.EventHandler(this.btn_adicionar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite o nome do Veiculo:";
+            // 
+            // tb_veiculo
+            // 
+            this.tb_veiculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_veiculo.Location = new System.Drawing.Point(12, 25);
+            this.tb_veiculo.Name = "tb_veiculo";
+            this.tb_veiculo.Size = new System.Drawing.Size(170, 26);
+            this.tb_veiculo.TabIndex = 2;
+            // 
+            // tb_listaVeiculos
+            // 
+            this.tb_listaVeiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_listaVeiculos.Location = new System.Drawing.Point(12, 57);
+            this.tb_listaVeiculos.Multiline = true;
+            this.tb_listaVeiculos.Name = "tb_listaVeiculos";
+            this.tb_listaVeiculos.Size = new System.Drawing.Size(251, 320);
+            this.tb_listaVeiculos.TabIndex = 3;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.Location = new System.Drawing.Point(12, 383);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(251, 29);
+            this.btn_limpar.TabIndex = 4;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 418);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.tb_listaVeiculos);
+            this.Controls.Add(this.tb_veiculo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_adicionar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Componentes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_adicionar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_veiculo;
+        private System.Windows.Forms.TextBox tb_listaVeiculos;
+        private System.Windows.Forms.Button btn_limpar;
+    }
+}
+
---
+++ VisualStudio/Componentes/Componentes/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            if(tb_veiculo.Text == "")
+            {
+                MessageBox.Show("Digite um veiculo!");
+                tb_veiculo.Focus();
+                return;
+            }
+            tb_listaVeiculos.Text += tb_veiculo.Text + ", ";
+            tb_veiculo.Clear();
+            tb_veiculo.Focus();
+
+
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            tb_listaVeiculos.Clear();
+        }
+    }
+}
